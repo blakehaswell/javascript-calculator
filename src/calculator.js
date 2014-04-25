d9c986199cab14b7dcf0791a3b6b6f9fd5955846
Blake Haswell <haswell00@gmail.com>
2014-04-25 18:20:20 +1000

    Hooked up calculator and display so that the display updates correctly as keys are pressed, including adding validation so that only valid keys can be pressed.

--- src/calculator.js
+++ src/calculator.js
@@ -16,5 +16,61 @@ calculator.addInitializer(function () {
 });
 
 keypad.on('keyPress', function (key) {
-    global.alert('Key press! ' + key.get('value'));
+    var equation = display.getText();
+    if (isValidKey(equation, key)) {
+        switch (key.get('type')) {
+        case 'clear':
+            equation = '';
+            break;
+        case 'equals':
+            equation = getResult(equation);
+            break;
+        default:
+            equation += key.get('value');
+        }
+    }
+    display.setText(equation);
 });
+
+function isValidKey(equation, key) {
+    var type = key.get('type');
+    var value = key.get('value');
+    if (type === 'clear' || (type === 'number' && value !== '.')) {
+        return true;
+    } else if (value === '.') {
+        if (isEquationEmpty(equation) ||
+                !isDecimalPlaceSinceLastOperator(equation)) {
+            return true;
+        }
+    } else if (value === '-') {
+        if (isEquationEmpty(equation) || equation.slice(-1) !== '-') {
+            return true;
+        }
+    } else if (type === 'operator' || type === 'equals') {
+        if (!isEquationEmpty(equation) && isLastCharNumber(equation)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isEquationEmpty(equation) {
+    return equation === '';
+}
+
+function isLastCharNumber(equation) {
+    if (isEquationEmpty(equation)) return false;
+    return !!equation.slice(-1).match(/[0-9]/);
+}
+
+function isDecimalPlaceSinceLastOperator(equation) {
+    var numbers = equation.split(/[+|-|\xD7|\xF7]/);
+    var lastNumber = numbers[numbers.length - 1];
+    console.log('Last number:', lastNumber, lastNumber.indexOf('.'));
+    return lastNumber.indexOf('.') !== -1;
+}
+
+function getResult(equation) {
+    equation = equation.replace(/\xD7/g, '*').replace(/\xF7/g, '/');
+    return eval(equation).toString();
+}
--- src/display.js
+++ src/display.js
@@ -1,4 +1,5 @@
 var ItemView = require('backbone.marionette').ItemView;
+var Model = require('backbone').Model;
 
 module.exports = ItemView.extend({
 
@@ -11,6 +12,24 @@ module.exports = ItemView.extend({
 
     template: function () {
         return '';
+    },
+
+    initialize: function () {
+        this.model = new Model({ text: '' });
+    },
+
+    getText: function () {
+        return this.model.get('text');
+    },
+
+    setText: function (value) {
+        return this.model.set('text', value);
+    },
+
+    modelEvents: {
+        'change:text': function () {
+            this.$el.val(this.getText());
+        }
     }
 
 });
